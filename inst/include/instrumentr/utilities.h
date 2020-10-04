261d16b0777cb457ccb1cdf65fa62adf23e9fa87
Aviral Goel <goel.aviral@gmail.com>
2020-10-03 22:24:40 -0400

    Remove API from utilities

--- inst/include/instrumentr/utilities.h
+++ inst/include/instrumentr/utilities.h
@@ -1,6 +1,4 @@
 #ifndef INSTRUMENTR_UTILITIES_H
 #define INSTRUMENTR_UTILITIES_H
 
-char* instrumentr_duplicate_string(const char* string);
-
 #endif /* INSTRUMENTR_UTILITIES_H */
--- src/utilities.c
+++ src/utilities.c
@@ -1,10 +1,1 @@
 #include <instrumentr/utilities.h>
-#include <string.h>
-#include <stdlib.h>
-
-char* instrumentr_duplicate_string(const char* original) {
-    int length = strlen(original);
-    char* duplicate = (char*) malloc(length * sizeof(char));
-    strcpy(duplicate, original);
-    return duplicate;
-}
